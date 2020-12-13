5383f969da3f6410878e5421ee1b3f2382f4e900
Kevin <macintoshera3@protonmail.ch>
2020-12-13 10:45:51 +0100

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,3 +47,14 @@ $(function() {
 		});
 
 });
+/*loader*/
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
